6ae4baee8c83e49c16478addbe6b6582a520ca7b
Raghad-Alim <145917753+Raghad-Alim@users.noreply.github.com>
2023-10-06 14:28:31 +0300

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,12 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Asus
- */
-public class Node {
+public class Node <T> {
+    public T data;
+    public Node<T> next ;
+    
+    public Node(){
+        data = null;
+        next= null;
+    }
+    public Node (T data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+    
     
 }
