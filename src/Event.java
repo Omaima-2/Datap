bfd3e1cf546ce4c924392fe25c022972ec998d0d
Raghad-Alim <145917753+Raghad-Alim@users.noreply.github.com>
2023-10-10 13:41:18 +0300

    Update Event.java

--- src/Event.java
+++ src/Event.java
@@ -1,9 +1,33 @@
 
 public class Event {
-    private String eventTitle;
+         private String eventTitle;
     private String dateAndTime;
     private String location;
-    private Contact cantact;
+    private Contact contact ;
+
+    public Event(String eventTitle, String dateAndTime, String location, Contact contact) {
+        this.eventTitle = eventTitle;
+        this.dateAndTime = dateAndTime;
+        this.location = location;
+        this.contact = contact;
+    }
+
+    public String getEventTitle() {
+        return eventTitle;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+ 
     
     
 }
