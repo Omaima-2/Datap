50ff6cdddc3b20ea51a798dde8edd05a8a46c10d
Raghad-Alim <145917753+Raghad-Alim@users.noreply.github.com>
2023-10-11 09:27:34 +0300

    Update Event.java

--- src/Event.java
+++ src/Event.java
@@ -1,33 +1,64 @@
 
 public class Event {
-         private String eventTitle;
-    private String dateAndTime;
-    private String location;
-    private Contact contact ;
+       private String eventTitle;
+    private String eventdate;
+    private String eventTime;
+    private String eventLocation;
+    private String contactName ;
+  
 
-    public Event(String eventTitle, String dateAndTime, String location, Contact contact) {
+    public Event(String eventTitle, String eventdate, String eventTime, String eventLocation, String contactName) {
         this.eventTitle = eventTitle;
-        this.dateAndTime = dateAndTime;
-        this.location = location;
-        this.contact = contact;
+        this.eventdate = eventdate;
+        this.eventTime = eventTime;
+        this.eventLocation = eventLocation;
+        this.contactName = contactName;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" + "eventTitle=" + eventTitle + ", eventdate=" + eventdate + ", eventTime=" + eventTime + ", eventLocation=" + eventLocation + ", contactName=" + contactName + '}';
     }
 
     public String getEventTitle() {
         return eventTitle;
     }
 
-    public String getDateAndTime() {
-        return dateAndTime;
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle = eventTitle;
     }
 
-    public String getLocation() {
-        return location;
+    public String getEventdate() {
+        return eventdate;
     }
 
-    public Contact getContact() {
-        return contact;
+    public void setEventdate(String eventdate) {
+        this.eventdate = eventdate;
     }
- 
-    
-    
+
+    public String getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(String eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public String getEventLocation() {
+        return eventLocation;
+    }
+
+    public void setEventLocation(String eventLocation) {
+        this.eventLocation = eventLocation;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+   
 }
+
